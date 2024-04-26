4afafce3dd123c7934dd063420c5e8c7133f1758
16Shadows <egor.0.00@mail.ru>
2024-04-26 20:01:02 +0500

    feat: Добавлен запрос списка игр.
    path: api/v1/games

--- src/controllers/GameController.ts
+++ src/controllers/GameController.ts
@@ -1,8 +1,11 @@
+import { ModelDataSource } from "../model/dataSource";
 import { Game } from "../model/game";
 import { Controller } from "../modules/core/controllers/decorators";
 import { MiddlewareBag } from "../modules/core/middleware/middleware";
 import { Return } from "../modules/core/mimeType/decorators";
 import { GET } from "../modules/core/routing/decorators";
+import { QueryArgument } from "../modules/core/routing/query";
+import { badRequest } from "../modules/core/routing/response";
 
 type GameInfo = {
     id: number;
@@ -14,8 +17,22 @@ type GameInfo = {
     images: string[];
 }
 
+type ListQuery = {
+    start: number;
+    count: number;
+};
+
 @Controller('api/v1/games')
 export class GameController extends Object {
+    static readonly ListCountHardLimit: number = 100;
+
+    private _dbContext: ModelDataSource;
+
+    constructor(dbContext: ModelDataSource) {
+        super();
+        this._dbContext = dbContext;
+    }
+
     @GET('{game:game}')
     @Return('application/json')
     async getGameInfo(bag: MiddlewareBag, game: Game): Promise<GameInfo> {
@@ -28,5 +45,39 @@ export class GameController extends Object {
             tags: (await game.tags).map(x => x.text),
             images: (await game.images).map(x => x.path)
         };
+    }
+
+    @GET()
+    @Return('application/json')
+    @QueryArgument('start', {
+        typeId: 'int',
+        canHaveMultipleValues: false,
+        optional: false
+    })
+    @QueryArgument('count', {
+        typeId: 'int',
+        canHaveMultipleValues: false,
+        optional: false
+    })
+    async getGamesList(bag: MiddlewareBag, query: ListQuery) {
+        if (query.count > GameController.ListCountHardLimit)
+            return badRequest([]);
+        
+        var repo = this._dbContext.getRepository(Game);
+
+        return await Promise.all((await repo.find({
+            skip: query.start,
+            take: query.count
+        })).map(async game => {
+            return {
+                id: game.id,
+                name: game.name,
+                playerCount: game.playerCount,
+                ageRating: game.ageRating,
+                description: game.description,
+                tags: (await game.tags).map(x => x.text),
+                images: (await game.images).map(x => x.path)
+            };
+        }));
     }
 }
