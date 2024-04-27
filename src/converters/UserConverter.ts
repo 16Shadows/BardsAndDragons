839516d10ecdd9a690210e6bfccb71d316ce9b53
16Shadows <egor.0.00@mail.ru>
2024-04-27 15:16:56 +0500

    feat: Добавлен UserConverter
    ITypeConverter для typeId user, определяет пользователя по username.

---
+++ src/converters/UserConverter.ts
@@ -0,0 +1,21 @@
+import { ModelDataSource } from "../model/dataSource";
+import { User } from "../model/user";
+import { Converter, ITypeConverter } from "../modules/core/converters/converter";
+
+@Converter('user')
+export class UserConverter implements ITypeConverter {
+    private _dbContext: ModelDataSource;
+
+    constructor(dbContext: ModelDataSource) {
+        this._dbContext = dbContext;
+    }
+    
+    async convertFromString(str: string): Promise<User | undefined> {
+        var repo = this._dbContext.getRepository(User);
+        var user = repo.findOneBy({
+            username: str
+        });
+
+        return user == null ? undefined : user;
+    }
+}
