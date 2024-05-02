c087a69c9d6b6172d567bc5735e695f4675a1dd8
Miraellax <catchit@ya.ru>
2024-05-02 21:33:59 +0500

    Поля для описания профиля и контактов

--- client/src/components/ProfilePage.tsx
+++ client/src/components/ProfilePage.tsx
@@ -32,30 +32,53 @@ const ProfilePage = () => {
   const [town, setTown] = useState<string | null>(null);
   const [birthDate, setBirthDate] = useState<Date | null>(null);
   const [isShowingAge, setIsShowingAge] = useState<boolean>(false);
+  const [profileDescription, setProfileDescription] = useState<string | null>(
+    null
+  );
+  const [profileContacts, setProfileContacts] = useState<string | null>(null);
 
   const handleNameChange = (event: {
     target: { value: React.SetStateAction<null | string> };
   }) => {
     if (event.target.value === "") setName(null);
     else setName(event.target.value);
-    // TODO запрос на изменение бд
+    // запрос на изменение бд идет при сохранении изменений
   };
   const handleTownChange = (event: {
     target: { value: React.SetStateAction<null | string> };
   }) => {
     setTown(event.target.value);
-    // TODO запрос на изменение бд
+    // запрос на изменение бд идет при сохранении изменений
   };
   const handleBirthDateChange = (value: React.SetStateAction<null | Date>) => {
     setBirthDate(value);
-    // TODO запрос на изменение бд
+    // запрос на изменение бд идет при сохранении изменений
   };
   const hangleIsShowingAgeChange = (event: {
     target: { value: React.SetStateAction<Boolean> };
   }) => {
     if (event.target.value === true) setIsShowingAge(true);
     else setIsShowingAge(false);
-    // TODO запрос на изменение бд
+    // запрос на изменение бд идет при сохранении изменений
+  };
+  const handleDescriptionChange = (event: {
+    target: { value: React.SetStateAction<null | string> };
+  }) => {
+    if (event.target.value === "") setProfileDescription(null);
+    else setProfileDescription(event.target.value);
+    // запрос на изменение бд идет при сохранении изменений
+  };
+  const handleContactsChange = (event: {
+    target: { value: React.SetStateAction<null | string> };
+  }) => {
+    if (event.target.value === "") setProfileContacts(null);
+    else setProfileContacts(event.target.value);
+    // запрос на изменение бд идет при сохранении изменений
+  };
+
+  const SaveChangesToDB = () => {
+    console.log("Все изменения были сохранены в БД.");
+    // TODO запрос на изменение бд этого конкретного пользователя
   };
 
   const navigate = useNavigate();
@@ -154,8 +177,8 @@ const ProfilePage = () => {
                 disabled={!isEditing}
                 name="town"
                 type="text"
-                placeholder="Город"
-                value={town ? town : "Город (не выбран)"}
+                placeholder="Не выбран"
+                value={town ? town : ""}
                 onChange={handleTownChange}
               ></input>
               {/* <DropdownList></DropdownList> */}
@@ -176,23 +199,73 @@ const ProfilePage = () => {
         <hr style={{ marginTop: 15 }} />
 
         <div className="row">
-          <div className="form-check col">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="isShowingAgeCheckbox"
-              checked={isShowingAge}
-            />
-            <label className="form-check-label" htmlFor="isShowingAgeCheckbox">
-              Показывать мой возраст
-            </label>
-            <TooltipComponent message='Если выбрано "Не показывать" - ваш Возраст не будет виден другим пользователям, но продолжит использоваться в алгоритме подбора игроков'></TooltipComponent>
+          <div className="col">
+            <div className="form-check col">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="isShowingAgeCheckbox"
+                checked={isShowingAge}
+              />
+              <label
+                className="form-check-label"
+                htmlFor="isShowingAgeCheckbox"
+              >
+                Показывать мой возраст
+              </label>
+              <TooltipComponent message='Если выбрано "Не показывать" - ваш Возраст не будет виден другим пользователям, но продолжит использоваться в алгоритме подбора игроков'></TooltipComponent>
+            </div>
           </div>
         </div>
 
         <hr style={{ marginTop: 15 }} />
 
+        <div className="row">
+          <div className="col">
+            <label htmlFor="ProfileDescription">Описание профиля</label>
+            <textarea
+              rows={4}
+              maxLength={300}
+              className="form-control"
+              id="ProfileDescription"
+              disabled={!isEditing}
+              value={profileDescription ? profileDescription : ""}
+              onChange={handleDescriptionChange}
+              placeholder="Опишите ваши интересы, предпочтения в играх и т.п."
+            ></textarea>
+            <small id="DescriptionHelpText" className="form-text text-red">
+              Описание профиля видно всем пользователям
+            </small>
+          </div>
+        </div>
+
+        <hr style={{ marginTop: 15 }} />
+        <div className="row">
+          <div className="col">
+            <label htmlFor="ProfileContacts">Контакты</label>
+            <textarea
+              rows={4}
+              maxLength={300}
+              className="form-control"
+              id="ProfileContacts"
+              disabled={!isEditing}
+              value={profileContacts ? profileContacts : ""}
+              onChange={handleContactsChange}
+              placeholder="Например, ссылка на аккаунт в Телеграме, ВКонтакте, адрес электронной почты..."
+            ></textarea>
+            <small id="DescriptionHelpText" className="form-text text-red">
+              <>
+                Пожалуйста, оставьте актуальные контакты, по которым другие
+                игроки смогут с вами связаться и позвать поиграть!
+              </>
+              <>Контакты видны только вашим друзьям</>
+            </small>
+          </div>
+        </div>
+        <hr style={{ marginTop: 15 }} />
+
         <div className="row mb-2">
+          {/* TODO сдвинуть кнопки в центр строки хоть как нибудь */}
           {isEditing ? (
             <Button
               key={"doneRedactingButton"}
@@ -209,6 +282,7 @@ const ProfilePage = () => {
             ></Button>
           )}
         </div>
+
         <div className="row">
           <Popup
             popupButtonText="Удалить профиль"
