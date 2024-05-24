b011ba68918b79199b6443460e56d1bc4ab92fe6
16Shadows <egor.0.00@mail.ru>
2024-05-24 14:58:27 +0500

    feat: Добавлен хук useDynamicList для кусочной загрузки списков
    Хук принимает функцию для преобразования текущего списка в следующее состояние.
    При изменении функции для преобразования списка хук очищает список и начинает обновлять его состояния заново.
    
    Хук возвращает текущее состояние списка и функцию для запроса перехода в следующее состояние. Множественный вызов функции перехода в следующее состояние приводит к последовательному переходу между состояниями.

---
+++ client/src/utils/useDynamicList.ts
@@ -0,0 +1,57 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export type ListStateConversionResult<ItemType> = {
+    isFinal: boolean;
+    list: ReadonlyArray<ItemType>;
+}
+
+export type ListStateConverter<ItemType> = {
+    (current: ReadonlyArray<ItemType>): Promise<ListStateConversionResult<ItemType>>;
+}
+
+/**
+ * Creates a dynamic list the state of which can be updated as needed
+ * @param nextState A function converting the current list into its next state
+ * @returns The result of converting to the next state which contains list - new list state - and isFinal - indicating whether this is the final state of the list with no further changes
+ */
+function useDynamicList<ItemType>(nextState: ListStateConverter<ItemType>): [ReadonlyArray<ItemType> | null, () => void] {
+    const [items, setItems] = useState<ReadonlyArray<ItemType> | null>(null);
+    const requestChain = useRef<Promise<ReadonlyArray<ItemType>>>(Promise.resolve([]));
+    const version = useRef(0);
+    const listFinalized = useRef(false);
+
+    useEffect(() => {
+        let localVersion = ++version.current;
+        listFinalized.current = false;
+        setItems(null);
+        requestChain.current = nextState([]).then(x => {
+            listFinalized.current = x.isFinal;
+            if (localVersion === version.current)
+                setItems(x.list);
+            return x.list;
+        });
+    }, [nextState]);
+
+    var requestNextBatch = useCallback(() => {
+        if (listFinalized.current === true)
+            return;
+
+        let localVersion = version.current;
+        
+        requestChain.current = requestChain.current.then(x => {
+            return listFinalized.current ? {list: x, isFinal: true} : nextState(x);
+        }).then(x => {
+            if (!listFinalized.current && localVersion === version.current)
+                setItems(x.list);
+
+            if (listFinalized.current !== x.isFinal)
+                listFinalized.current = x.isFinal;
+
+            return x.list;
+        });
+    }, [nextState]);
+
+    return [items, requestNextBatch];
+}
+
+export default useDynamicList;
