e706748431f350d0a54c8cf05eaf777ce590e9fd
Danil Fedoseev <fs159@mail.ru>
2024-05-27 03:20:27 +0500

    feat: добавлен интерфейс для данных игрока

---
+++ client/src/pages/PlayersPage/PlayerData.ts
@@ -0,0 +1,10 @@
+export interface PlayerData {
+    matchId: number;
+    username: string;
+    displayName: string;
+    age?: number;
+    city?: string;
+    description: string;
+    avatarPath?: string;
+    games: string[];
+}
