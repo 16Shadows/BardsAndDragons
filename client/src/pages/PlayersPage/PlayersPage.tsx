afd14980fe29a3afa3fcc18019ffe9e61986b8ba
Danil Fedoseev <fs159@mail.ru>
2024-05-27 16:29:29 +0500

    feat: добавлены запросы подтверждения отклонения мэтча на фронт

--- client/src/pages/PlayersPage/PlayersPage.tsx
+++ client/src/pages/PlayersPage/PlayersPage.tsx
@@ -1,62 +1,90 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {Col, Row} from "react-bootstrap";
 import PlayerCard from "./PlayerCard";
 import {PlayerData} from "./PlayerData";
-import usePlayersApi from "./usePlayersApi";
+import {errorLoadingData} from "../../utils/errorMessages";
+import useApi from "../../http-common";
 
 const PlayersPage = () => {
     const [matches, setMatches] = useState<PlayerData[]>([]);
     const [currentMatchId, setCurrentMatchId] = useState(0);
 
-    const {getMatches} = usePlayersApi();
+    const api = useApi();
 
-    useEffect(() => {
-        getMatches().then((data) => {
-            setMatches(data);
+    // Function to show error message
+    const showError = (message: string) => {
+        alert(message);
+    };
+
+    // Function to get matches from API
+    const getMatches = useCallback(async () => {
+        try {
+            const response = await api.get('matching/get-players');
+            setMatches(response.data);
             setCurrentMatchId(0);
-        });
+        } catch {
+            showError(errorLoadingData);
+        }
+    }, [api, showError]);
+
+    // Function to handle next match
+    const handleNextMatch = useCallback(async () => {
+        if (currentMatchId < matches.length - 1) {
+            // Move to the next match
+            setCurrentMatchId(currentMatchId + 1);
+        } else {
+            // If it's the last match, fetch new matches
+            await getMatches();
+        }
+    }, [currentMatchId, matches.length, getMatches]);
+
+    // Function to handle player accept
+    const handleAccept = useCallback(async (username: string) => {
+        try {
+            // Send friend request
+            await api.post(`user/${username}/addFriend`);
+            await handleNextMatch();
+        } catch {
+            showError(errorLoadingData);
+        }
+    }, [api, handleNextMatch, showError]);
+
+    // Function to handle player reject
+    const handleReject = useCallback(async (username: string) => {
+        try {
+            // Send friend request
+            await api.post(`matching/${username}/rejectMatch`);
+            await handleNextMatch();
+        } catch {
+            showError(errorLoadingData);
+        }
+    }, [api, handleNextMatch, showError]);
+
+    // Get matches on page load
+    useEffect(() => {
+        getMatches().catch(() => showError(errorLoadingData));
     }, []);
 
-    const handleAccept = (username: string) => {
-        console.log(`Player accepted: ${username}`);
+    // TODO: add page with explanation what to do when there are no matches
+    // TODO: add is loading state
+    if (!matches.length || !matches[currentMatchId]) {
+        return (
+            <div>
+                <h1>Нет мэтчей!</h1>
+            </div>
+        );
+    }
 
-        // Обработка принятия мэтча
-        if (currentMatchId < matches.length - 1) {
-            setCurrentMatchId(currentMatchId + 1);
-        } else {
-            // Если это последний мэтч, выполнить соответствующую обработку
-            alert('All players accepted!');
-            setCurrentMatchId(0);
-        }
-
-    };
-
-    const handleReject = (username: string) => {
-        console.log(`Player rejected ${username}`);
-
-        // Обработка отклонения мэтча
-        if (currentMatchId < matches.length - 1) {
-            setCurrentMatchId(currentMatchId + 1);
-        } else {
-            // Если это последний мэтч, выполнить соответствующую обработку
-            alert('All players rejected!');
-            setCurrentMatchId(0);
-        }
-    };
-
+    // TODO: prohibit matching if user hasn't filled all required fields in profile
+    // Render player card
     return (
         <Row className="justify-content-md-center">
             <Col md="8">
-                {matches[currentMatchId] && (() => {
-                    const playerData = matches[currentMatchId];
-                    return (
-                        <PlayerCard
-                            {...playerData}
-                            onAccept={handleAccept}
-                            onReject={handleReject}
-                        />
-                    );
-                })()}
+                <PlayerCard
+                    {...matches[currentMatchId]}
+                    onAccept={handleAccept}
+                    onReject={handleReject}
+                />
             </Col>
         </Row>
     );
--- client/src/pages/PlayersPage/usePlayersApi.ts
+++ client/src/pages/PlayersPage/usePlayersApi.ts
@@ -1,20 +1,4 @@
-import {PlayerData} from "./PlayerData";
-import useApi from "../../http-common";
-
 const usePlayersApi = () => {
-    const api = useApi();
-    const getMatches = (): Promise<PlayerData[]> => {
-        return new Promise((resolve, reject) =>
-            api.get('matching/get-players')
-                .then((response) => {
-                    resolve(response.data);
-                })
-                .catch((error) => {
-                    reject(error);
-                }));
-    };
-
-    return {getMatches};
 };
 
 export default usePlayersApi;
