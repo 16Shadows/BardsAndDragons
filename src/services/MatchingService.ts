ca2965be09b0760741621e099d325f30a1b4d811
Danil Fedoseev <fs159@mail.ru>
2024-05-30 01:09:12 +0500

    feat: написан запрос для получения данных мэтчей по критериям, запрос проверки может ли пользователь участвовать в мэтчинге

--- src/services/MatchingService.ts
+++ src/services/MatchingService.ts
@@ -6,6 +6,24 @@ import {UsersFriend} from "../model/usersFriend";
 import {RejectedMatch} from "../model/rejectedMatch";
 import {SelectQueryBuilder} from "typeorm";
 
+type GameData = {
+    name: string;
+    playsOnline: boolean;
+}
+
+export type PlayerData = {
+    // Only used for counting matches. Not connected with ID in database
+    matchId: number;
+
+    username: string;
+    displayName: string;
+    age?: number;
+    city?: string;
+    description: string;
+    avatarPath?: string;
+    games: GameData[];
+}
+
 @injectable()
 export class MatchingService {
     // Points for calculating the score for each potential player
@@ -24,48 +42,38 @@ export class MatchingService {
         this._dbContext = dbContext;
     }
 
-    async getPotentialPlayers(currentUser: User): Promise<any[]> {
-        const potentialPlayersQuery = this.getPotentialPlayersQuery(currentUser.id);
-        return await potentialPlayersQuery.getRawMany();
-    }
-
-    async getRankedPlayers(currentUser: User): Promise<any[]> {
-        const potentialPlayersQuery = this.getPotentialPlayersQuery(currentUser.id);
-        const rankedPlayersQuery = this.getRankedPlayersQuery(potentialPlayersQuery, currentUser.id);
-        return await rankedPlayersQuery.getRawMany();
+    /**
+     * Gets ranked players with player data for matching.
+     */
+    async getRankedPlayersForMatching(currentUser: User, limit: number): Promise<PlayerData[]> {
+        const matchingCandidatesQuery = this.getMatchingCandidatesQuery(currentUser.id);
+        const rankedPlayersQuery = this.getRankedPlayersQuery(matchingCandidatesQuery, currentUser.id, limit);
+        return await rankedPlayersQuery.getRawMany<PlayerData>();
     }
 
     /**
-     * Returns a query builder for the list of potential players for the current user
+     * Returns a query builder for the list of users who are allowed to participate in matching for the current user.
+     * Chooses players with required data. Excludes friends and rejected matches.
      */
-    private getPotentialPlayersQuery(currentUserId: number): SelectQueryBuilder<User> {
+    private getMatchingCandidatesQuery(currentUserId: number): SelectQueryBuilder<User> {
         const repo = this._dbContext.getRepository(User);
-        return repo
+        const qb = repo
             .createQueryBuilder("user")
+
             // Select only user id
             .select("user.id", "id")
             .distinct(true)
 
-            .where("user.id != currentUser.id") // exclude the current user
-            .andWhere("user.isDeleted = false") // only active users
+            // Exclude the current user
+            .where("user.id != currentUser.id")
 
-            .andWhere("user.birthday IS NOT NULL") // only users with a birthday
-            .andWhere("user.displayName IS NOT NULL AND user.displayName != ''") // only users with a display name
-            .andWhere("user.avatarId IS NOT NULL") // only users with an avatar
-            .andWhere("user.profileDescription IS NOT NULL AND user.profileDescription != ''") // only users with a profile description
-            .andWhere("user.contactInfo IS NOT NULL AND user.contactInfo != ''") // only users with a contact info
-            .andWhere("user.cityId IS NOT NULL") // only users with a city
+            // Set the current user
+            .addFrom(User, "currentUser")
+            .andWhere("currentUser.id = :currentUserId", {currentUserId});
 
-            // Only users with at least one game
-            .andWhere(qb => {
-                const subQuery = qb.subQuery()
-                    .select("usersGame.userId")
-                    .from(UsersGame, "usersGame")
-                    .where("usersGame.userId = user.id")
-                    .getQuery();
-                return `EXISTS ${subQuery}`;
-            })
+        this.applyMatchingCriteria(qb);
 
+        return qb
             // Exclude users who are already friends with the current user
             .andWhere(qb => {
                 const subQuery = qb.subQuery()
@@ -87,17 +95,53 @@ export class MatchingService {
                     .getQuery();
                 return `NOT EXISTS ${subQuery}`;
             })
+    }
 
-            // Set the current user
-            .addFrom(User, "currentUser")
-            .andWhere("currentUser.id = :currentUserId", {currentUserId})
+    /**
+     * Checks if a user with the given ID meets the matching criteria.
+     */
+    private async isUserValidForMatching(userId: number): Promise<boolean> {
+        const repo = this._dbContext.getRepository(User);
+        const qb = repo
+            .createQueryBuilder("user")
+            .where("user.id = :userId", {userId});
+
+        this.applyMatchingCriteria(qb);
+
+        const user = await qb.getOne();
+        return !!user;
+    }
+
+    /**
+     * Applies the criteria for users to participate in matching.
+     */
+    private applyMatchingCriteria(qb: SelectQueryBuilder<User>): SelectQueryBuilder<User> {
+        return qb
+            .andWhere("user.isDeleted = false") // only active users
+
+            .andWhere("user.birthday IS NOT NULL") // only users with a birthday
+            .andWhere("user.displayName IS NOT NULL AND user.displayName != ''") // only users with a display name
+            .andWhere("user.avatarId IS NOT NULL") // only users with an avatar
+            .andWhere("user.profileDescription IS NOT NULL AND user.profileDescription != ''") // only users with a profile description
+            .andWhere("user.contactInfo IS NOT NULL AND user.contactInfo != ''") // only users with a contact info
+            .andWhere("user.cityId IS NOT NULL") // only users with a city
+
+            // Only users with at least one game
+            .andWhere(qb => {
+                const subQuery = qb.subQuery()
+                    .select("usersGame.userId")
+                    .from(UsersGame, "usersGame")
+                    .where("usersGame.userId = user.id")
+                    .getQuery();
+                return `EXISTS ${subQuery}`;
+            });
     }
 
     // TODO: add a selection of players according to the parameters:
     //  5. The more game tags match, the higher the score
     //  6. Mix a random number?
     /**
-     * Returns a query builder for the list of ranked players for the current user.
+     * Returns a query builder for the list of ranked players for matching for the current user.
      *
      * Rank is based on:
      * 1. Same cities
@@ -105,48 +149,80 @@ export class MatchingService {
      * 3. More common games, the higher the score
      * 4. More common friends, the higher the score
      */
-    private getRankedPlayersQuery(potentialPlayersQuery: SelectQueryBuilder<User>, currentUserId: number) {
+    private getRankedPlayersQuery(matchingCandidatesQuery: SelectQueryBuilder<User>, currentUserId: number, limit: number): SelectQueryBuilder<User> {
         const repo = this._dbContext.getRepository(User);
 
-        // Add subqueries for city and age scores
-        this.addCityScoreSubquery(potentialPlayersQuery);
-        this.addAgeScoreSubquery(potentialPlayersQuery);
+        // Add selection for city and age scores
+        this.addCityScoreSelection(matchingCandidatesQuery);
+        this.addAgeScoreSelection(matchingCandidatesQuery);
 
         const query = repo
             .createQueryBuilder("user")
-            .select("user.username", "username")
             // Sum all scores
-            .addSelect(`
+            .select(`
                 COALESCE("potentialPlayers"."cityScore", 0) +
                 COALESCE("potentialPlayers"."ageScore", 0) +
                 COALESCE("gameScoreSubquery"."gameScore", 0) +
                 COALESCE("friendScoreSubquery"."friendScore", 0)
             `, "totalScore")
-            .innerJoin(`(${potentialPlayersQuery.getQuery()})`, "potentialPlayers", `"potentialPlayers"."id" = "user"."id"`);
+            .addSelect(`
+                ROW_NUMBER() OVER (ORDER BY 
+                    COALESCE("potentialPlayers"."cityScore", 0) +
+                    COALESCE("potentialPlayers"."ageScore", 0) +
+                    COALESCE("gameScoreSubquery"."gameScore", 0) +
+                    COALESCE("friendScoreSubquery"."friendScore", 0)
+                        DESC)`, "matchId")
+            .innerJoin(`(${matchingCandidatesQuery.getQuery()})`, "potentialPlayers", `"potentialPlayers"."id" = "user"."id"`);
 
+        // Add subqueries for game and friend scores
         const gameScoreSubquery = this.getGameScoreSubquery(query, currentUserId);
         const friendScoreSubquery = this.getFriendScoreSubquery(query, currentUserId);
+
+        this.selectPlayerData(query);
+
         return query
             .leftJoin(`(${gameScoreSubquery.getQuery()})`, "gameScoreSubquery", `"gameScoreSubquery"."userId" = "user"."id"`)
             .leftJoin(`(${friendScoreSubquery.getQuery()})`, "friendScoreSubquery", `"friendScoreSubquery"."userId" = "user"."id"`)
-            .orderBy(`"totalScore"`, "DESC")
+            .limit(limit)
             // Set parameters for calculation of scores
             .setParameter("pointsForSameCity", this.pointsForSameCity)
             .setParameter("pointsForSameAge", this.pointsForSameAge)
     }
 
     /**
-     * Calculate the score for the same city as the current user. If cities are the same, user gets constant points
+     * Adds the player data to the query builder.
      */
-    private addCityScoreSubquery(qb: SelectQueryBuilder<User>) {
+    private selectPlayerData(qb: SelectQueryBuilder<User>): SelectQueryBuilder<User> {
+        return qb
+            .leftJoin('user.games', 'usersGame')
+            .leftJoin('usersGame.game', 'game')
+            .leftJoin('user.city', 'city')
+            .leftJoin('user.avatar', 'avatar')
+
+            .addSelect([
+                'user.username AS "username"',
+                'user.displayName AS "displayName"',
+                `EXTRACT(YEAR FROM AGE(user.birthday)) AS "age"`,
+                'city.name AS "city"',
+                'user.profileDescription AS "description"',
+                'avatar.blob AS "avatarPath"'
+            ])
+            .addSelect(`json_agg(json_build_object('name', game.name, 'playsOnline', usersGame.playsOnline))`, 'games')
+            .groupBy('user.id, avatar.blob, city.name, "totalScore"')
+    }
+
+    /**
+     * Calculates the score for the same city as the current user. If cities are the same, user gets constant points.
+     */
+    private addCityScoreSelection(qb: SelectQueryBuilder<User>) {
         qb
             .addSelect(`CASE WHEN "user"."cityId" = "currentUser"."cityId" THEN :pointsForSameCity ELSE 0 END`, "cityScore")
     }
 
     /**
-     * Calculate the score for the same age as the current user. The bigger the difference, the fewer points will be given to the user
+     * Calculates the score for the same age as the current user. The bigger the difference, the fewer points will be given to the user.
      */
-    private addAgeScoreSubquery(qb: SelectQueryBuilder<User>) {
+    private addAgeScoreSelection(qb: SelectQueryBuilder<User>) {
         qb
             .addSelect(
                 `:pointsForSameAge - ABS(EXTRACT(YEAR FROM "user"."birthday") - EXTRACT(YEAR FROM "currentUser"."birthday"))`,
@@ -154,7 +230,7 @@ export class MatchingService {
     }
 
     /**
-     * Calculate the score for the same games as the current user. For each same game, user gets constant points
+     * Calculates the score for the same games as the current user. For each same game, user gets constant points.
      */
     private getGameScoreSubquery(qb: SelectQueryBuilder<User>, currentUserId: number) {
         return qb
@@ -169,7 +245,7 @@ export class MatchingService {
     }
 
     /**
-     * Calculate the score for the same friends as the current user. For each same friend, user gets constant points
+     * Calculates the score for the same friends as the current user. For each same friend, user gets constant points.
      */
     private getFriendScoreSubquery(qb: SelectQueryBuilder<User>, currentUserId: number) {
         return qb
