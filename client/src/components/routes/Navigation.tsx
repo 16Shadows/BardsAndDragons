cdee2d823788be8262846f7a9b2e3d1f93d162ba
Danil Fedoseev <fs159@mail.ru>
2024-05-31 20:40:25 +0500

    feat: добавлены ссылки для путей в роутах

--- client/src/components/routes/Navigation.tsx
+++ client/src/components/routes/Navigation.tsx
@@ -1,9 +1,25 @@
+export function getHomeRoute() {
+    return "/";
+}
+
 export function getLoginPageRoute() {
     return "/login";
 }
 
+export function getRegistrationPageRoute() {
+    return "/register";
+}
+
+export function getMyProfilePageRoute() {
+    return "/my-profile";
+}
+
 export function getFriendsPageRoute() {
     return "/my-friends";
+}
+
+export function getMyGamesPageRoute() {
+    return "/my-games";
 }
 
 export function getUserProfileRoute(username: string) {
@@ -12,4 +28,12 @@ export function getUserProfileRoute(username: string) {
 
 export function getPlayersPageRoute() {
     return "/players";
+}
+
+export function getGamesPageRoute() {
+    return "/games";
+}
+
+export function getNotFoundRoute() {
+    return "*";
 }
