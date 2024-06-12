46dbd4e20ee28da37e006aeac12b79b6adb047b9
NickP59 <ppocht1029@yandex.ru>
2024-06-12 20:34:22 +0500

    картинка на странице 404 + небольшое изменение оформления

--- client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import "./NotFoundPage.css"; 
+import "./NotFoundPage.css";
+import picture404 from "../../resources/page404.png"
+import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
-    return ( 
-        // TODO: придумать прикол для оформления 404
+    return (
         <div className="not-found-container">
+            <img
+                src={picture404}
+                alt="404"
+                className="not-found-image"
+                style={{ width: '500px', height: 'auto' }}
+            />
             <div className="not-found-text">
-            <h1>О нет... Мы потеряли страницу...</h1>
-            <p>Шаловливые гоблины опять все украли!</p>
-            <p>Не беспокойтесь, исследователи уже отправились на ее поиски.</p>
+                <h2>О нет... Мы потеряли страницу...</h2>
+                <p>Шаловливые гоблины опять все украли! </p>
+                <Link to="/">
+                    <button className="home-button">
+                        ВПЕРЕД ИСКАТЬ!
+                    </button>
+                </Link>
+
             </div>
         </div>
     );
