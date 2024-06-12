c6c7d7bf911c73c70a50dc8eea285e9177133ab3
NickP59 <ppocht1029@yandex.ru>
2024-06-12 21:24:13 +0500

    fix

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
 
-
+// TODO: Добавить ссылки!!!
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-links">
-          <a href="#">политика конфы и условия</a>
+           {/* поменять ссылку! */}
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=PLetg744TF10BrdPjaEXf4EsJ1wz6fyf95&index=1">политика конфиденциальности и условия</a>
         </div>
+
         <div className="footer-support">
           <span className="highlight">Если вы хотите поддержать наш проект</span>
-          <span>"номер карты и сvс"</span>
+          <span>"номер карты и cvc"</span>
         </div>
+
         <div className="footer-contact">
-          <span className="highlight">Проверка - напишите нам, если у вас проблема или предложение</span>
-          <a href="#">почта администрации</a>
+          <span className="highlight">Поддержка - напишите нам, если у вас проблема или предложение</span>
+           {/* поменять ссылку! */}
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=PLetg744TF10BrdPjaEXf4EsJ1wz6fyf95&index=1">почта администрации</a>
+
           <span></span>
-          <a href="#">ссылка на офф. группу в ВК</a>
+           {/* поменять ссылку! */}
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=PLetg744TF10BrdPjaEXf4EsJ1wz6fyf95&index=1">ссылка на офф. группу в BK</a>
         </div>
+
       </div>
+
       <div className="footer-copyright">
         <span>BardsAndDragons Copyright © 2024 BardsAndDragons - All rights reserved</span>
       </div>
+
     </footer>
   );
 };
